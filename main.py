eefd864e74c97b7be7a796c9e0d82ff732963968
Fyodortelega <morozov.fyodor.71@mail.ru>
2026-01-12 00:12:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
+import asyncio
 from telegram import Bot
 
 TOKEN = os.getenv("TOKEN")
 CHANNEL = os.getenv("CHANNEL")
 
-print("BOT STARTED")
+async def main():
+    bot = Bot(token=TOKEN)
+    await bot.send_message(chat_id=CHANNEL, text="✅ Бот работает через Render")
 
-bot = Bot(token=TOKEN)
-bot.send_message(chat_id=CHANNEL, text="✅ Бот работает через Render")
+asyncio.run(main())
