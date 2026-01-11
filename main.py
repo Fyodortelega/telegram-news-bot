958728194c81e8aaac6d94b9a94c6e950474df5f
Fyodortelega <morozov.fyodor.71@mail.ru>
2026-01-12 00:29:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ from telegram import Bot
 TOKEN = os.getenv("TOKEN")
 CHANNEL = os.getenv("CHANNEL")
 
+print("TOKEN:", TOKEN)
+print("CHANNEL:", CHANNEL)
+
 async def main():
+    if not TOKEN or not CHANNEL:
+        print("❌ Переменные окружения пустые!")
+        return
+
     bot = Bot(token=TOKEN)
     await bot.send_message(chat_id=CHANNEL, text="✅ Бот работает через Render")
-    await bot.session.close()  # закрываем сессию, чтобы Render не ругался
+    await bot.session.close()
 
-# запускаем асинхронно
 asyncio.run(main())
