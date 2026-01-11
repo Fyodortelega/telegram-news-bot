fdf3278dfd285ac400b3476caf89542b2ee17e0f
Fyodortelega <morozov.fyodor.71@mail.ru>
2026-01-11 23:58:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from telegram import Bot
 TOKEN = os.getenv("TOKEN")
 CHANNEL = os.getenv("CHANNEL")
 
-print("TOKEN:", TOKEN)
-print("CHANNEL:", CHANNEL)
+print("BOT STARTED")
 
 bot = Bot(token=TOKEN)
-bot.send_message(chat_id=CHANNEL, text="✅ Бот успешно запущен")
+bot.send_message(chat_id=CHANNEL, text="✅ Бот работает через Render")
